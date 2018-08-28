9ebc59abd6eb6e01dfe935f78e52ba4889ad4068
fbonhomme <franck.bonhomme@gmail.com>
2018-08-28 18:42:44 +0200

    First

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const routes = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+app.use('/users', users);
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.listen(3000,()=>{console.log('connected on port 3000')})
+
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/register', (req,res)=>{
+    res.render('register');
+});
+
+router.get('/login', (req,res)=>{
+    res.render('login');
+});
+
+router.get('/logout', (req,res)=>{
+    res.render('logout');
+});
+
+module.exports = router;
