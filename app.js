0d4675eb756c4a3eacd1326af30e0e7866471be3
fbonhomme <franck.bonhomme@gmail.com>
2018-09-28 18:17:26 +0200

    ajout error

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ const users = require('./routes/users');
 const mongoose = require('mongoose');
 
 const app = express();
+
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost:27017/gestiopass',{useNewUrlParser:true})
-        .then(() => {console.log('MongoDB Connected ...')},
-        err => {console.log(err)}
-        );
+        .then(() => console.log('MongoDB Connected ...'))
+        .catch(err => console.log(err))
+
+// Load User Model
+
+require('./models/user');
+const User = mongoose.model('User');   
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
@@ -24,8 +29,20 @@ app.use('/users', users);
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-/*app.get('/',(req,res)=>{
-    res.render('home')
-})*/
-app.listen(3000,()=>{console.log('connected on port 3000')})
+app.post('/users',(req, res) => {
+   let errors = [];
+   if(!req.body.email){
+       errors.push({text: "Stop!!! email please !!!"});
+   }
+   if(errors.length > 0){
+       res.render('register', {
+           errors : errors,
+           email: req.body.email
+       })
+   } else{
+       res.send('passed');
+   }
+});
 
+
+app.listen(3000,()=>{console.log('connected on port 3000')})
